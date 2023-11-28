ea66920df826a6446cfb750955d6a3ca3e6ed3d3
Wendell47 <ps4.wendell@gmail.com>
2023-11-28 10:10:27 -0400

    fix (2): add more animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var timeDisplay = document.getElementById("time");
-//var day = document.getElementById("day");
-var dateIn = document.getElementById("date");
-
+let timeDisplay = document.getElementById("time");
+//let day = document.getElementById("day");
+let dateIn = document.getElementById("date");
+let eye = document.querySelectorAll(".eye");
 
  const Mouths = [
 
@@ -63,7 +63,32 @@ function refreshTime() {
 
   
 }
+
+function eyeAnimation(){
+  console.log("passou")
+  for (let i = 0; i < eye.length; i++){
+    eye[i].classList.add('eyeAnimation')
+  }
+
+}
+
+//setInterval(eyeAnimation, 6000)
+
+/*setInterval(() => {
+  for (let i = 0; i < eye.length; i++){
+    eye[i].classList.remove('eyeAnimation')
+  }
+ }, 8000)*/
+
 setInterval(refreshTime, 1000)
 
 
 
+
+let load = document.querySelector(".loading");
+
+// Registra uma função anônima para o evento DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Muda a mensagem de status para "Pronto!"
+  load.classList.add("hide")
+});
